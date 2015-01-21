8577661fc6dd26777629196671b41fa9c4e92980
iero <greg@iero.org>
2015-01-21 10:27:38 +0100

    Update
    ieroFullDataLogger works

---
+++ ieroFullDataLogger/ieroFullDataLogger.ino
@@ -0,0 +1,155 @@
+/*
+  SD card datalogger
+ 	 
+ */
+#include <SPI.h>
+#include <dht.h>
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+#define LOG_INTERVAL  5000 // mills between entries
+
+#define dht22Pin 2  // DHT22 on analog pin 2 
+dht DHT;            // DHT22 (temperature and humidity)
+RTC_Millis rtc;     // Real Time Clock
+
+File logFile;
+
+void error(char *str) {
+  //Serial.print("error: ");
+  //Serial.println(str);
+  while(1);
+}
+
+void setup() {
+  //Serial.begin(9600);
+  //while (!Serial) { ; }  // wait for serial port to connect. Needed for Leonardo only
+
+  // RTC setup
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+
+  //Serial.print("Initializing SD card...");
+  pinMode(SS, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(10, 11, 12, 13)) {
+  //  Serial.println("Card failed, or not present");
+    while (1) ;    // don't do anything more:
+  }
+  //Serial.println("Card initialized.");
+    
+  // fetch the time for file name
+  DateTime now = rtc.now();
+  String dirNameString = "";
+  String fileNameString = "";
+
+  dirNameString += now.year();
+  if (now.month() < 10) {
+    dirNameString += "0" ; 
+    dirNameString += now.month();
+  } else dirNameString += now.month();
+  if (now.day() < 10) {
+    dirNameString += "0" ; 
+    dirNameString += now.day();
+  } else dirNameString += now.day();
+  
+  char direname[dirNameString.length()+1];
+  dirNameString.toCharArray(direname, sizeof(direname));
+  if ( ! SD.exists(direname)) {
+    SD.mkdir(direname) ;
+    //Serial.println("Directory "+dirNameString+" created");
+  } //else Serial.println("Directory "+dirNameString+" exists");
+  
+  fileNameString += dirNameString+"/";
+  
+  if (now.hour() < 10) {
+    fileNameString += "0" ; 
+    fileNameString += now.hour();
+  } else fileNameString += now.hour();
+  if (now.minute() < 10) {
+    fileNameString += "0" ; 
+    fileNameString += now.minute();
+  } else fileNameString += now.minute();
+  if (now.second() < 10) {
+    fileNameString += "0" ; 
+    fileNameString += now.second();
+  } else fileNameString += now.second();
+    
+  // create a new file
+  fileNameString += ".csv";
+  char filename[fileNameString.length()+1];
+  fileNameString.toCharArray(filename, sizeof(filename));
+  logFile = SD.open(filename, FILE_WRITE); 
+  Serial.println("File "+fileNameString+" created");
+  
+  if (! logFile) {
+    error("couldnt create file");
+  }
+}
+
+void loop() {  
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+
+  // Read DHT22 sensor
+  int chk = DHT.read22(dht22Pin);
+  
+  //Serial.print("Temperature : ");    
+  //Serial.println(DHT.temperature);
+  //Serial.print("Humidite : ");    
+  //Serial.println(DHT.humidity);
+  
+  // fetch the time
+  DateTime now = rtc.now();
+    
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // log time
+  dataString += now.year();
+  dataString += "/";
+  if (now.month() < 10) {
+    dataString += "0" ; 
+    dataString += now.month();
+  } else dataString += now.month();
+  dataString += "/";
+  if (now.day() < 10) {
+    dataString += "0" ; 
+    dataString += now.day();
+  } else dataString += now.day();
+  dataString += " ";
+  if (now.hour() < 10) {
+    dataString += "0" ; 
+    dataString += now.hour();
+  } else dataString += now.hour();
+  dataString += ":";
+  if (now.minute() < 10) {
+    dataString += "0" ; 
+    dataString += now.minute();
+  } else dataString += now.minute();
+  dataString += ":";
+  if (now.second() < 10) {
+    dataString += "0" ; 
+    dataString += now.second();
+  } else dataString += now.second();
+    
+  
+  
+  dataString += ",";
+
+  // log dht22 sensor
+  dataString += DHT.temperature;
+  dataString += ",";
+  dataString += DHT.humidity;
+
+  // write log
+  logFile.println(dataString);
+
+  // print to the serial port too:
+  //Serial.println(dataString);
+  
+  logFile.flush();  
+}
+
+
---
+++ test_dht/test_dht.ino
@@ -0,0 +1,105 @@
+#include <SD.h>
+#include "dht.h"
+
+#define DHTPIN 2     // what pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+dht DHT;
+
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
+// NOTE: For working with a faster chip, like an Arduino Due or Teensy, you
+// might need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// Example to initialize DHT sensor for Arduino Due:
+//DHT dht(DHTPIN, DHTTYPE, 30);
+
+void setup(
+// initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename); ) {
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+ 
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit
+  float f = dht.readTemperature(true);
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index
+  // Must send in temp in Fahrenheit!
+  float hi = dht.computeHeatIndex(f, h);
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hi);
+  Serial.println(" *F");
+}
+
+
